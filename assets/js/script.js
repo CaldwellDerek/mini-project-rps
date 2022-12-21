b2e47c0a55fba75c0c484253910f51858890ac31
Derek Caldwell <djohncaldwell@gmail.com>
2022-12-21 12:27:08 -0800

    added score tracking

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,11 @@ let computerOptions = ["R", "P", "S"];
 function computerChoice() {
     return computerOptions[Math.floor(Math.random() * 3)];
 }
+// Variables to keep track of results
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
 
 // Asks if the user would like to play the game.
 if (window.confirm("Would you like to play Rock, Paper Scissors?")){
@@ -14,7 +19,7 @@ if (window.confirm("Would you like to play Rock, Paper Scissors?")){
     while (keepPlaying) {
 
         // User's entered choice
-        let choice = window.prompt("Enter R, P, or S");
+        let choice = window.prompt("Enter R, P, or S").toUpperCase;
         // Computer's choice
         let opponentChoice = computerChoice();
 
@@ -24,18 +29,23 @@ if (window.confirm("Would you like to play Rock, Paper Scissors?")){
             if (choice === 'R' || choice === 'P' || choice === 'S'){
                 isBad = false;
             } else {
-                choice = window.prompt("Your input differed from R, P, or S. Please only enter only a single letter of either R, P, or S.");
+                choice = window.prompt("Your input differed from R, P, or S. Please only enter only a single letter of either R, P, or S.").toUpperCase;
             }
         }
 
-        // Confirm Results
+        // Confirm and display results
         if ((choice === 'R' && opponentChoice === 'S') || (choice === 'P' && opponentChoice === 'R') || (choice === 'S' && opponentChoice === 'P')){
-            window.alert(`Your opponent chose ${opponentChoice}, you win!`);
+            window.alert(`You chose ${choice} and your opponent chose ${opponentChoice}, you win!`);
+            wins++;
         } else if (choice === opponentChoice) {
-            window.alert(`Your opponent chose ${opponentChoice}, you tied!`);
+            window.alert(`You chose ${choice} and your opponent chose ${opponentChoice}, you tied!`);
+            ties++;
         } else {
-            window.alert(`Your opponent chose ${opponentChoice}, you lost!`);
+            window.alert(`You chose ${choice} and your opponent chose ${opponentChoice}, you lost!`);
+            losses++;
         }
+        window.alert(`Your current record is --- Wins: ${wins}, Ties: ${ties}, and Losses: ${losses}.`);
+
 
         // Confirms if user would like to keep playing
         if (!window.confirm("Would you like to keep playing?")){
